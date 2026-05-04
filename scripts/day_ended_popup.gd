c1248bdaee7868e05bce60fe55629d0ffa2e095b
ruben-velasquez <rualvemo@hotmail.com>
2026-05-04 20:44:52 -0300

    feat: add game over menu

--- scripts/day_ended_popup.gd
+++ scripts/day_ended_popup.gd
@@ -6,12 +6,10 @@ class_name DayEndedPopUp
 func setup():
 	var daily_costs = GameManager.daily_costs
 	var money_amount = GameManager.current_score
-	var new_money_amount = money_amount
 	var output = ""
 	
 	for fee in daily_costs:
-		new_money_amount -= fee.amount
 		output += "%s: [color=red]-%s$[/color]\n" % [fee.name.capitalize(), fee.amount]
 	
-	output += "Money: %s$" % [new_money_amount]
+	output += "Money: %s$" % [money_amount]
 	info.text = output
--- scripts/ui_manager.gd
+++ scripts/ui_manager.gd
@@ -1,14 +1,26 @@
 extends CanvasLayer
+class_name UIManager
 
 @export var finish_popup: DayEndedPopUp
+@export var game_over_screen: Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	finish_popup.process_mode = Node.PROCESS_MODE_DISABLED
-	finish_popup.hide()
-	GameManager.on_timer_end.connect(show_finish_popup)
+	hide_window(finish_popup)
+	hide_window(game_over_screen)
+	GameManager.ui_manager = self
 
-func show_finish_popup():
-	finish_popup.process_mode = Node.PROCESS_MODE_INHERIT
-	finish_popup.show()
+func show_end_day_popup():
+	show_window(finish_popup)
 	finish_popup.setup()
+
+func show_game_over_screen():
+	show_window(game_over_screen)
+
+func hide_window(_c: Control):
+	_c.process_mode = Node.PROCESS_MODE_DISABLED
+	_c.hide() 
+
+func show_window(_c: Control):
+	_c.process_mode = Node.PROCESS_MODE_INHERIT
+	_c.show()
