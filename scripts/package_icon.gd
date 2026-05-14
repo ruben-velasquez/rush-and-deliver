7070d5faaff7990937870fcc12c17d8bc88c5747
ruben-velasquez <rualvemo@hotmail.com>
2026-05-14 20:15:20 -0300

    fix: make reward labels of packages work

--- scripts/package_icon.gd
+++ scripts/package_icon.gd
@@ -2,6 +2,7 @@ class_name PackageIcon
 extends Control
 
 @export var progress_bar: TextureProgressBar
+@export var reward_label: RichTextLabel
 
 @export var fragile_icon: Texture2D
 @export var heavy_icon: Texture2D
@@ -22,12 +23,15 @@ func _process(_delta: float) -> void:
 	match package.property:
 		Package.PackageProperty.URGENT:
 			progress_bar.value = (package.urgent_time_left * 100) / RunData.stats.urgent_packages_time
+			reward_label.text = "$%s" % package.reward
 		Package.PackageProperty.FRAGILE:
 			progress_bar.value = ((RunData.stats.fragile_packages_health - package.fragile_health) * 100) / RunData.stats.fragile_packages_health
 
 func setup(_package: Package):
 	package = _package
 	progress_bar.value = 0
+	
+	reward_label.text = "$%s" % package.reward
 	
 	match package.property:
 		Package.PackageProperty.URGENT:
