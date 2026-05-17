52bec6def9ed4776545b53db4bd450a2bd6eb323
ruben-velasquez <rualvemo@hotmail.com>
2026-05-17 14:16:08 -0300

    feat: add ring shader for package deliver zone

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,12 +1,32 @@
 extends Area2D
 class_name Goal
 
+const WAIT_TIME_SEG = 1
+
+@export var sprite: Sprite2D
+
 var appear_range: int = 500
 var package: Package
+var player_on_zone: bool = false
+var player_time = 0
 
 func _on_body_entered(_body: Node2D) -> void:
-	GameManager.packages_manager.deliver(package)
-	queue_free()
+	player_time = 0
+	player_on_zone = true
+
+func _on_body_exited(_body: Node2D) -> void:
+	player_time = 0
+	player_on_zone = false
+
+func _process(delta: float) -> void:
+	if player_on_zone:
+		player_time += delta
+	
+	if player_time >= WAIT_TIME_SEG:
+		GameManager.packages_manager.deliver(package)
+		queue_free()
+	
+	sprite.self_modulate = lerp(Color.WHITE, Color.LAWN_GREEN, player_time/WAIT_TIME_SEG)
 
 func set_goal_zone():
 	position.x = randi_range(-appear_range, appear_range)
