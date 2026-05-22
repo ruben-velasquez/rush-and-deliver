54dc2c3c69e495cac9a7656a589c32c62a4c8de6
ruben-velasquez <rualvemo@hotmail.com>
2026-05-21 22:50:30 -0300

    feat(ui): add dynamic color to the estimated costs label

--- scripts/estimated_costs_label.gd
+++ scripts/estimated_costs_label.gd
@@ -6,6 +6,17 @@ func _ready() -> void:
 	update_estimated()
 	GameManager.packages_manager.on_fail_package.connect(update_estimated)
 	GameManager.player.on_crash.connect(update_estimated)
+	GameManager.on_money_updated.connect(update_estimated)
 
 func update_estimated():
-	text = "Estimated Costs: [font=%s]$%s" % [MONEY_FONT, GameManager.get_estimated_costs()]
+	var money_text: String
+	var estimated_costs = GameManager.get_estimated_costs()
+	
+	if RunData.money < estimated_costs:
+		money_text = "[color=RED]$%s[/color]" % [estimated_costs]
+	else:
+		money_text = "[color=GREEN]$%s[/color]" % [estimated_costs]
+	
+	money_text = "[outline_size=7][outline_color=WHITE]%s[/outline_color][/outline_size]" % money_text
+	
+	text = "Estimated Costs: [font=%s]%s" % [MONEY_FONT, money_text]
