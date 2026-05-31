8b7a3dd82be08e0d658f4d39a4e301d1115dcd2f
ruben-velasquez <rualvemo@hotmail.com>
2026-05-31 12:26:12 -0300

    fix: normalize light direction of shadows

--- scenes/object_shadow.gd
+++ scenes/object_shadow.gd
@@ -1,20 +1,28 @@
 extends Sprite2D
 
-const light_dir = Vector2(-0.05, 0.05)
+const light_dir = Vector2(-1, 1)
 var _owner: Node2D
 var pos_offset: Vector2
+
+@export var is_static: bool = false
 
 func _ready() -> void:
 	material = material.duplicate(true)
 	_owner = get_parent()
-	pos_offset = _owner.global_position - global_position
+	
+	if !is_static:
+		pos_offset = position
+	
 	ShadowsViewport.instance.append_shadow(self)
 
 func _process(_delta: float) -> void:
 	if !_owner.is_visible_in_tree(): return hide()
 	else: show()
 	
-	global_position = _owner.global_position + pos_offset
+	if is_static:
+		global_position = _owner.global_position
+	else:
+		global_position = _owner.global_position + pos_offset
 	global_rotation = _owner.global_rotation
 	
 	global_position -= ShadowsViewport.instance.global_position
